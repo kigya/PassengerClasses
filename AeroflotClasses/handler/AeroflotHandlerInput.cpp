124488d07525f3694bc46c468491e81dcf3e08a1
kigya <kirillborichevskiy@gmail.com>
2021-09-21 02:59:11 +0300

    Create AeroflotHandlerInput

---
+++ AeroflotClasses/handler/AeroflotHandlerInput.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Borichevskiy Kirill
+// Belarusian State University 2021
+//
+//
+
+#include "AeroflotHandlerInput.h"
---
+++ AeroflotClasses/handler/AeroflotHandlerInput.h
@@ -0,0 +1,16 @@
+//
+// Created by Borichevskiy Kirill
+// Belarusian State University 2021
+//
+//
+
+#ifndef AEROFLOTCLASSES_AEROFLOTHANDLERINPUT_H
+#define AEROFLOTCLASSES_AEROFLOTHANDLERINPUT_H
+
+
+class AeroflotHandlerInput {
+
+};
+
+
+#endif //AEROFLOTCLASSES_AEROFLOTHANDLERINPUT_H
